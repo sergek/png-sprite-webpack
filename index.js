13815be553e5daabd12aa1aa6510762649c00885
sergek <passenger.k@gmail.com>
2015-12-10 18:00:34 +0300

    Add proper hooks for webpack

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 var Sprite = require('./lib/sprite-webpack');
 var _ = require('lodash');
+var Promise = require('bluebird');
 
 function SpriteWebpackPlugin(options) {
   var opt = Sprite.options;
   this.options = _.assign(opt, options);
 }
 
-SpriteWebpackPlugin.prototype.apply = function() {
+SpriteWebpackPlugin.prototype.apply = function(compiler) {
   var self = this;
   var opt = self.options;
   var newOpt = _.clone(opt, true);
-
+  // Options for 2x scaled images
   newOpt = _.assign(newOpt, {
     source: opt.source + 'scaled-at-200/',
     spriteName: opt.spriteName + '@2x'
   });
 
+  compiler.plugin('run', function(compiler, callback) {
+    self.compileSprite(opt, newOpt, callback)
+  });
+  var watchStarted = false;
+  compiler.plugin('watch-run', function (watcher, watchRunCallback) {
+    if (watchStarted) {
+      return watchRunCallback();
+    }
+    watchStarted = true;
+    self.compileSprite(opt, newOpt, watchRunCallback)
+  });
+};
+
+SpriteWebpackPlugin.prototype.compileSprite = function(opt, newOpt, cb) {
   Sprite.createStyles(opt);
-  Sprite.createImage(opt);
-  Sprite.createImage(newOpt);
-  Sprite.addImport(opt);
+  Promise.resolve().then(function() {
+    console.log('Generate sprites')
+    return Sprite.createImage(opt);
+  }).then(function() {
+    console.log('Generate sprites @2x')
+    return Sprite.createImage(newOpt);
+  }).then(function() {
+    Sprite.addImport(opt);
+    cb();
+  });
 };
 
 module.exports = SpriteWebpackPlugin;
